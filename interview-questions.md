bcbb6f8c6d935c06156ded87cff0de6263286758
DeeMo44 <demario326@gmail.com>
2023-04-17 11:50:17 -0400

    complete

--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,80 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: 
+Calling super in a ruby class gives tells the parent class that it can borrow information from it
+like variables and methods.
 
 Researched answer:
+The super keyword is used to access a method with the same name from the super class. The class using the
+super keyword passes the arguments to the parent class. This allows developers to have greater organization.
+
+https://www.rubyguides.com/2018/09/ruby-super-keyword/
 
 2. What is a gem?
 
-Your answer:
+Your answer: 
+A gem is a bundle of dependencies for a ruby file.
 
 Researched answer:
+A gem in Ruby is a package of Ruby software that gives a developer custom methods. The methods 
+in the gem are designed to speed up specific tasks. For example, the date gem gives a developer
+access to the date method which could be used creating a calendar or timeline.
+
+https://github.com/ruby/date/blob/master/README.md
 
 3. What is a relational database? Are there other kinds of databases?
 
 Your answer:
+A relational database stores info that are related to each other and can be accessed by its rows and columns.
 
 Researched answer:
+A relational database houses data points that are related to each other. For example, in PostgreSql, we have
+data points that are all related to countries (population, name, gnp, head of state, etc.)
+
+https://www.oracle.com/database/what-is-a-relational-database/
 
 4. What are primary keys? Why are they important?
 
 Your answer:
+In Ruby on Rails, a primary key is the id number of an object in a database. It is important because each key is
+unique.
 
 Researched answer:
+A primary key, or primary keyword, is a unique identifier for a row of data in a table. If were to create a database
+with all of my cohorts names, one column would have an ID number, and the other with their names. Each ID number would
+be unique because each person is their own person.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
 Your answer:
+The HTTP Put, Get, -(I forgot this one)-, and delete. They correlate (in this order) to the CRUD actions as Create,
+Read, Update, and Delete. 
 
 Researched answer:
+The HTTP verbs and CRUD actions corresponds to each other as such:
+POST = CREATE ==> create entity
+GET = READ ==>  retrieve entity
+PUT = UPDATE ==>  update entity
+DELETE = DELETE ==> delete entity
+
+https://www.learnhowtoprogram.com/c-and-net/basic-web-applications/http-crud-methods#:~:text=HTTP%20Request%20Methods,with%20a%20different%20HTTP%20method.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
 1. Model validations:
+Validating that the model has achieved its goal.
 
 2. RESTful routes:
+There are 7 restful routes that corresponds with a controller#action. the 7 routes perform HTTP verbs.
 
 3. ERB:
+ERB is a plain text source code file.
 
 4. Params:
+Params means parameters. Parameters in Ruby are variables used in methods to be manipulated.
 
 5. API:
+An API (application programming interface) is software that allows different software to communicate.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,23 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+# create a method named letter_contain that takes an array and a string as parameters
+def letter_contain(array, string)
+  # use .select to iterate over the array and remove values that do not meet certain conditions
+  # use variable word for the string being iterated over
+  # use .include? to see if the word has the string and print 
+  puts array.select {|word| word.include?(string)}
+end
+
+letter_contain(beverages_array, letter_o)
+# Output: 
+# coffee
+# soda water
+
+letter_contain(beverages_array, letter_t)
+# Output: 
+# water
+# soda water
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,13 +34,69 @@ letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def hash_value(hash)
+  puts hash.values.sort
+end
+
+hash_value(us_states)
+# Output: 
+# California
+# Arizona
+# Nevada
+# Maine
+# New Hampshire
+# Rhode Island
+# Washington
+# Oregon
+# Idaho
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+# create a class called bike
+class Bike 
+  # Bike is initialized with the bikes model name
+  def initialize(model)
+    # by default, the bike starts at 0 speed and has 2 wheels. @model recieves the name from model
+    @wheels = 2
+    @current_speed = 0
+    @model = model
+  end
+  # bike_info is a class method that recieves the initialized variables and prints 
+  # a string when invoked using string interpolation
+  def bike_info
+    p"The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+  end
+  # pedal_faster is a class method that recieves a number in the 'speed' parameter
+  def pedal_faster(speed)
+    # we only want positive numbers for increased speed so it has to be greater 
+    # than zero
+    if(speed>0)
+      # we add the speed number on to our current_speed
+     p @current_speed += speed
+    end
+  end
+  # brake is a class method that also recieves the speed parameter
+  # in this case it will be the speed deducted from our current_speed
+  def brake(speed)
+    # if the current_speed - speed would be less than zero - then just make
+    # current speed 0, also we want only positive numbers
+    if(@current_speed - speed < 0 && speed > 0)
+     p @current_speed = 0
+    else
+     p @current_speed -= speed
+    end
+  end
+end
 
+my_bike = Bike.new('Huffy')
+my_bike.bike_info            #"The Huffy bike has 2 wheels and is going 0 mph"
 
+my_bike.pedal_faster(10)    #10
+my_bike.bike_info           #"The Huffy bike has 2 wheels and is going 10 mph"
 
+my_bike.brake(5)            #5
+my_bike.bike_info           #"The Huffy bike has 2 wheels and is going 5 mph"
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster(10) => 10
